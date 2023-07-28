3620906731413fc6d19de60afb1cedbb5af3b253
Lim_Jin_Xin_Hannah <s10243399@connect.np.edu.sg>
2023-07-29 01:30:39 +0800

    centred some textviews + added some icon png for UI

--- app/src/main/java/sg/edu/np/mad/pawgress/Fragments/Profile/UserSettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/pawgress/Fragments/Profile/UserSettingsActivity.java
@@ -36,14 +36,12 @@ public class UserSettingsActivity extends AppCompatActivity implements View.OnCl
         Button themeButton = findViewById(R.id.findTheme);
         Button profilePictureButton = findViewById(R.id.editProfilePicture);
         Button notificationsButton = findViewById(R.id.Notifs);
-        Button appModesButton = findViewById(R.id.appMode);
         TextView backButton = findViewById(R.id.backButton);
         Button capyModeButton = findViewById(R.id.capyMode);
 
         themeButton.setOnClickListener(this);
         profilePictureButton.setOnClickListener(this);
         notificationsButton.setOnClickListener(this);
-        appModesButton.setOnClickListener(this);
         backButton.setOnClickListener(this);
         capyModeButton.setOnClickListener(this);
     }
